35c9cd34b624b724ac473e6a0b376333dc4464f6
Risgabh Nigam <rishabhnigam491@gmail.com>
2023-08-13 20:32:01 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./Component/Header"
+import Meme from "./Component/Meme"
+function App() {
+  return (
+      <div>
+       <Header />
+       <Meme />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import logo from "../img/troll-face-png.png"
+import "../Css/Header.css"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={logo}
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
